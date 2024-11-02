23bc3ef589f25997caab2e7209c805db6a5aa9da
Pritee Gupta <priteegupta517@gmail.com>
2024-11-02 13:17:26 +0530

    initial commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,10 +7,10 @@ function Navbar() {
         <nav className="navbar">
             <div className="logo">Company Logo</div>
             <ul className="nav-links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Employees</a></li>
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">Logout</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Employees</a></li>
+                <li><a href="">Settings</a></li>
+                <li><a href="">Logout</a></li>
             </ul>
             <div className="user-info">
                 <span>Fakhar Naveed</span>
